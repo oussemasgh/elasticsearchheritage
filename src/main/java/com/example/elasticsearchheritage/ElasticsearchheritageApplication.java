d6d81fa955f4e15e6d9012c0a3acf088e4b93f0a
oussemasgh <oussema.sghaier@etudiant-enit.utm.tn>
2023-11-21 10:40:08 +0100

    Initial commit

---
+++ src/main/java/com/example/elasticsearchheritage/ElasticsearchheritageApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchheritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchheritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchheritageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elasticsearchheritage/ElasticsearchheritageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchheritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchheritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
